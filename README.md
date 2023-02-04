d3e653af7cba49b3ef306b598c489a2d6a96904d
gregor Feigel <gregor.feigel@icloud.com>
2023-02-04 12:12:44 +0100

    Initial Commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# ncx
+
+A description of this package.
---
+++ Tests/ncxTests/ncxTests.swift
@@ -0,0 +1,11 @@
+import XCTest
+@testable import ncx
+
+final class ncxTests: XCTestCase {
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct
+        // results.
+        XCTAssertEqual(ncx().text, "Hello, World!")
+    }
+}
